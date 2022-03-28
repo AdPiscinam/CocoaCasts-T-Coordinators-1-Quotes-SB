985888f289e1cd02b06f29ef39d73e4e8981ffe9
AdPiscinam <lab@walimaloui.com>
2022-03-28 13:33:38 +0200

    Fix Indentation

--- CocoaCasts-T-Coordinators-1-Quotes-SB/View Controllers/QuotesViewController.swift
+++ CocoaCasts-T-Coordinators-1-Quotes-SB/View Controllers/QuotesViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class QuotesViewController: UIViewController {
-
+    
     // MARK: - Properties
     
     private let quotes: [Quote] = [
-        Quote(author: "Marie Curie",        content: "Be less curious about people and more curious about ideas."),
-        Quote(author: "Albert Einstein",    content: "Life is like riding a bicycle. To keep your balance you must keep moving."),
-        Quote(author: "Jane Goodall",       content: "Every individual matters. Every individual has a role to play. Every individual makes a difference."),
-        Quote(author: "Dalai Lama",         content: "Be kind whenever possible. It is always possible."),
-        Quote(author: "Margaret Atwood",    content: "A word after a word after a word is power."),
-        Quote(author: "Bill Gates",         content: "Success is a lousy teacher. It seduces smart people into thinking they can't lose."),
-        Quote(author: "J.K. Rowling",       content: "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light."),
-        Quote(author: "Steve Jobs",         content: "Design is not just what it looks like and feels like. Design is how it works."),
-        Quote(author: "Rosalind Franklin",  content: "Science and everyday life cannot and should not be separated."),
-        Quote(author: "Charles Darwin",     content: "A man who dares to waste one hour of time has not discovered the value of life.")
+        Quote(author: "Marie Curie", content: "Be less curious about people and more curious about ideas."),
+        Quote(author: "Albert Einstein", content: "Life is like riding a bicycle. To keep your balance you must keep moving."),
+        Quote(author: "Jane Goodall", content: "Every individual matters. Every individual has a role to play. Every individual makes a difference."),
+        Quote(author: "Dalai Lama", content: "Be kind whenever possible. It is always possible."),
+        Quote(author: "Margaret Atwood", content: "A word after a word after a word is power."),
+        Quote(author: "Bill Gates", content: "Success is a lousy teacher. It seduces smart people into thinking they can't lose."),
+        Quote(author: "J.K. Rowling", content: "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light."),
+        Quote(author: "Steve Jobs", content: "Design is not just what it looks like and feels like. Design is how it works."),
+        Quote(author: "Rosalind Franklin", content: "Science and everyday life cannot and should not be separated."),
+        Quote(author: "Charles Darwin", content: "A man who dares to waste one hour of time has not discovered the value of life.")
     ]
     
     // MARK: -
@@ -73,7 +73,7 @@ class QuotesViewController: UIViewController {
     
     // MARK: - Actions
     
-    @IBAction func settings(_ sender: Any) {
+    @IBAction func settings(_ sender: UIBarButtonItem) {
         guard let settingsViewController = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: SettingsViewController.storyboardIdentifier) as? SettingsViewController else {
             fatalError("Unable to Instantiate Settings View Controller")
         }
@@ -83,7 +83,7 @@ class QuotesViewController: UIViewController {
     }
     
     // MARK: - Helper Methods
-
+    
     private func setupNotificationHandling() {
         NotificationCenter.default.addObserver(forName: .fontSizeDidChange, object: nil, queue: .main) { [weak self] _ in
             self?.tableView.reloadData()
@@ -93,7 +93,7 @@ class QuotesViewController: UIViewController {
             self?.tableView.reloadData()
         }
     }
-
+    
 }
 
 extension QuotesViewController: UITableViewDataSource {
@@ -112,7 +112,7 @@ extension QuotesViewController: UITableViewDataSource {
         
         // Configure Cell
         cell.configure(author: quote.author, content: quote.content)
-
+        
         return cell
     }
     
