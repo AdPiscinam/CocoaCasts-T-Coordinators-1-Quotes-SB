b3b3425a1e6506861db94b39121608047fd0fb85
AdPiscinam <lab@walimaloui.com>
2022-03-28 17:43:59 +0200

    Create Storyboardable Protocol

---
+++ CocoaCasts-T-Coordinators-1-Quotes-SB/Protocols/Storyboardable.swift
@@ -0,0 +1,10 @@
+// Storyboardable.swift
+// CocoaCasts-T-Coordinators-1-Quotes-SB
+// Created by Ad Piscinam on 28/03/2022
+// Copyright © 2022 Code Foundry. All rights reserved.
+
+import UIKit
+
+protocol Storyboardable {
+    
+}
